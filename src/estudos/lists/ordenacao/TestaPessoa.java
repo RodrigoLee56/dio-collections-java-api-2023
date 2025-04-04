095eb07663da53d1349f5fc9d624ab6cc00e2839
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-04 00:05:20 -0400

    Testar os métodos adicionarPessoa e ordenarPorIdade

--- src/estudos/lists/ordenacao/TestaPessoa.java
+++ src/estudos/lists/ordenacao/TestaPessoa.java
@@ -1,4 +1,16 @@
 package src.estudos.lists.ordenacao;
 
 public class TestaPessoa {
+    public static void main(String[] args) {
+
+
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+    }
 }
