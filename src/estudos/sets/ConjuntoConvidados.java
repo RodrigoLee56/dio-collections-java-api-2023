689b060a650ca9020322028080545e5c2c8a2045
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-04 01:10:54 -0400

    Implementar a classe ConjuntoConvidados e o método adicionarConvidado

--- src/estudos/sets/ConjuntoConvidados.java
+++ src/estudos/sets/ConjuntoConvidados.java
@@ -1,4 +1,16 @@
 package src.estudos.sets;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConjuntoConvidados {
+    private Set<Convidado> convidados;
+
+    public ConjuntoConvidados() {
+        this.convidados = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidados.add(new Convidado(codigoConvite, nome));
+    }
 }
