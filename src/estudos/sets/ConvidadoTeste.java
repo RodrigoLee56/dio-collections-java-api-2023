c1b0007509bc1314746f817c46985e61552c0b56
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-07 14:40:14 -0400

    Implementar os testes na classe ConvidadoTeste

--- src/estudos/sets/ConvidadoTeste.java
+++ src/estudos/sets/ConvidadoTeste.java
@@ -3,7 +3,28 @@ package src.estudos.sets;
 public class ConvidadoTeste {
     public static void main(String[] args) {
         ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
-        conjuntoConvidados.adicionarConvidado("Rodrigo", 100);
-        System.out.println(conjuntoConvidados.getConvidados());
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Alice", 1234);
+        conjuntoConvidados.adicionarConvidado("Bob", 1235);
+        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
+        conjuntoConvidados.adicionarConvidado("David", 1236);
+
+        // Exibindo os convidados no conjunto
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+
+        // Exibindo o número atualizado de convidados no conjunto
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        // Removendo um convidado do conjunto por código de convite
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
+
+        // Exibindo os convidados atualizados no conjunto
+        System.out.println("Convidados no conjunto após a remoção:");
+        conjuntoConvidados.exibirConvidados();
+
     }
 }
